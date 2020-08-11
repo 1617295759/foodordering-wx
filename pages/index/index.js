2f9a30206bb0f7c1550a9953d38dc3c4f068e263
Yuzhe Wang <1617295759@qq.com>
2020-08-11 09:09:12 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,98 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: '三餐邮你',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  //点击用户登陆按钮，转到用户页面，更改状态
+  StudentLog: function(e){
+    wx.switchTab({
+      url: '../userindex/userindex',
+    })
+    try {
+      wx.setStorageSync('userstate', '1')
+    } catch (e) { }
+  },
+  //点击管理员登陆按钮，转到管理员页面，更改用户状态
+  AdmiLog: function(e){
+    wx.navigateTo({
+      url: '../admindex/admindex',
+    })
+    try {
+      wx.setStorageSync('userstate', '3')
+    } catch (e) { }
+  },
+  onLoad: function () {
+
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse){
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      //在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+     }
+     //根据存储中的用户状态转到相应页面
+     try {
+      var value = wx.getStorageSync('userstate')
+      if (value) {
+        switch(value){
+          case "1": 
+            wx.switchTab({
+             url: '../userindex/userindex',
+            })
+            break
+          case"2":
+            wx.navigateTo({
+              url: '../userindex/userindex',
+            }) 
+          break
+        }
+      }else{
+        wx.setStorage({
+          key:"userstate",
+          data:"0"
+        })
+      }
+    } catch (e) {
+      // Do something when catch error
+    }
+  },
+  getUserInfo: function(e) {
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+
+})
